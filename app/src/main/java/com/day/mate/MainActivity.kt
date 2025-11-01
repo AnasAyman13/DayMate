3708429b78c0d4b6f8b25a9a0c8a2ddc946f3596
Anas Ayman El-Gebaili <anasaymanelgebaili@gmail.com>
2025-11-02 00:24:17 +0200

    Fix typo in README and update splash screen layout

--- app/src/main/java/com/day/mate/MainActivity.kt
+++ app/src/main/java/com/day/mate/MainActivity.kt
@@ -3,6 +3,8 @@ package com.day.mate
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
 import androidx.navigation.compose.rememberNavController
 
 class MainActivity : ComponentActivity() {
@@ -13,4 +15,9 @@ class MainActivity : ComponentActivity() {
             NavGraph(navController)
         }
     }
+}
+
+@Composable
+fun Foo(modifier: Modifier = Modifier) {
+    
 }
