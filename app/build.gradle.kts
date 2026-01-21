433d5d57ac194f35333927373ae1c7662222523a
Anas Ayman El-Gebaili <anasaymanelgebaili@gmail.com>
2026-01-21 19:13:05 +0200

    Updates

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -24,7 +24,7 @@ android {
         applicationId = "com.day.mate"
         minSdk = 29
         targetSdk = 36
-        versionCode = 4
+        versionCode = 5
         versionName = "1.0"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
